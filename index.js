65debd125bbdb5f4655eccf891b03f3f640faed5
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-19 05:37:23 -0400

    commit tree 3811abab9dc8509a33f4f669451fb60d46a4cc0b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dropoffBlock){
+    //return Math.abs(44-dropoffBlock)
+    if (dropoffBlock<=42){
+        return 42-dropoffBlock
+        }else{
+            return dropoffBlock - 42
+        }
+}
+
+function distanceFromHqInFeet(dropoffBlock){
+    return distanceFromHqInBlocks(dropoffBlock) * 264
+}
+
+function distanceTravelledInFeet(start,destination){
+//return Math.abs(start-destination)*264
+if (start > destination){
+    return (start-destination) * 264
+}else {
+    return (destination-start) * 264
+}
+}
+
+function calculatesFarePrice(start, destination){
+const distance = distanceTravelledInFeet(start, destination)
+if  (distance < 400){
+return 0
+}else if (distance > 400 && distance <=2000){
+    return .02 * (distance - 400)
+}else if (distance > 2000 && distance < 2500){
+    return 25 
+}else{
+    return 'cannot travel that far' 
+}
+}
